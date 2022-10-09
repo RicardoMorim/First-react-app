03a778b733ab119ffb03c22683e18242be56c8d1
RicardoMorim <ricardomorim05@gmail.com>
2022-10-09 16:07:12 +0100

    Components Part 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/menucomponent"
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+          <Menu/>
     </div>
   );
 }
