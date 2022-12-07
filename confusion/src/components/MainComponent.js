fe7b675b42585543c202eee2d5f554e33ecf106b
RicardoMorim <ricardomorim05@gmail.com>
2022-12-07 23:16:20 +0000

    second assignment

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Menu from "./menucomponent";
 import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import DishDetail from "./DishDetailComponent";
+import About from "./AboutComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
 import { COMMENTS } from "../shared/coments";
@@ -61,6 +62,7 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
